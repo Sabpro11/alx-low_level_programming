728ecf0954b0cd61b5443cb1f78cff5ab9e7ac7c
fatima-ezzahra <abahafatimaezzahra@gmail.com>
2023-03-16 13:48:32 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@ int b;
 long c;
 long long d;
 float e;
-printf("size of a char: %lu byte(s)\n", sizeof(a));
-printf("size of an int: %lu byte(s)\n", sizeof(b));
-printf("size of a long: %lu byte(s)\n", sizeof(c));
-printf("size of a long long: %lu byte(s)\n", sizeof(d));
-printf("size of a long long float: %lu byte(s)\n", sizeof(e));
+printf("size of a char: %lu byte(s)\n", Sizeof(a));
+printf("size of an int: %lu byte(s)\n", Sizeof(b));
+printf("size of a long: %lu byte(s)\n", Sizeof(c));
+printf("size of a long long: %lu byte(s)\n", Sizeof(d));
+printf("size of a float: %lu byte(s)\n", Sizeof(e));
 return (0);
 }
