a2e63dd9b84450fbfdbac2c31d6aff66a28c44c5
fatima-ezzahra <abahafatimaezzahra@gmail.com>
2023-03-16 11:54:30 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - A C program that prints a line with printf function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
