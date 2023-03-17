73289a03ae54cfd28c76dba804ccb3f58fb656fa
fatima-ezzahra <abahafatimaezzahra@gmail.com>
2023-03-17 12:28:44 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - A a program that prints all single digit numbers of base 10
+ * Return: 0 (success)
+ */
+int main(void)
+{
+int a;
+for (a = 0; a < 10; a++)
+putchar("%d", a);
+putchar('\n');
+return (0);
+}
